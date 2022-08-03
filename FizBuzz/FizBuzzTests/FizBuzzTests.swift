c34674fabd414a3da48b79b2ba678762464f3cd6
Paul Zabelin <paulz@users.noreply.github.com>
2022-08-03 16:41:38 -0700

    fizz buzz

--- FizBuzz/FizBuzzTests/FizBuzzTests.swift
+++ FizBuzz/FizBuzzTests/FizBuzzTests.swift
@@ -18,7 +18,22 @@ extension Int {
 }
 
 func getArrayOfStringsFromUpperBound(_ upperBound: Int) -> [String] {
-    (1...upperBound).map {String($0)}
+
+    var array: [String] = []
+    for i in 1...upperBound {
+
+        if i.isMultipleOfThreeFive() {
+            array.append("FizzBuzz")
+        } else if i.isMultipleOfThree() {
+            array.append("Fizz")
+        } else if i.isMultipleOfFive() {
+            array.append("Buzz")
+        } else {
+            array.append(String(i))
+        }
+    }
+    return array
+   // (1...upperBound).map {String($0)}
 }
 
 class FizBuzzTests: XCTestCase {
@@ -37,16 +52,14 @@ class FizBuzzTests: XCTestCase {
         XCTAssertTrue(15.isMultipleOfThreeFive())
     }
 
-    func testArrayOutputStrings() {
-        let array = getArrayOfStringsFromUpperBound(3)
-        let result = ["1", "2", "3"]
-        XCTAssertEqual(array, result)
+    func testFizzForMultiplesOfThree() {
+        XCTAssertEqual(getArrayOfStringsFromUpperBound(3), ["1", "2", "Fizz"])
     }
 
-    func testArrayOutputStringsForFour() {
-        let array = getArrayOfStringsFromUpperBound(4)
-        let result = ["1", "2", "3", "4"]
-        XCTAssertEqual(array, result)
+    func testBuzzForMultiplesOfFive() {
+        XCTAssertEqual(getArrayOfStringsFromUpperBound(5), ["1", "2", "Fizz", "4", "Buzz"])
     }
-
+    func testFizBuzzForMultiplesOfFiveAndThree() {
+        XCTAssertEqual(getArrayOfStringsFromUpperBound(15), ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"])
+    }
 }
