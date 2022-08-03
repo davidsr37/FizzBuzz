634a1bdbe252ae7b17ac76309ea2aee72b8cf595
David Rogers <David.Rogers37@T-Mobile.com>
2022-08-03 15:43:19 -0700

    Removed more blank setup code.

--- FizBuzz/FizBuzzTests/FizBuzzTests.swift
+++ FizBuzz/FizBuzzTests/FizBuzzTests.swift
@@ -5,11 +5,4 @@ import XCTest
 
 class FizBuzzTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 }
