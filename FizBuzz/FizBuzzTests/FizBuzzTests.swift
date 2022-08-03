bbe8416b2826927888f5fffffa1265ccaff84525
Paul Zabelin <paulz@users.noreply.github.com>
2022-08-03 15:56:33 -0700

    add multiple of three

--- FizBuzz/FizBuzzTests/FizBuzzTests.swift
+++ FizBuzz/FizBuzzTests/FizBuzzTests.swift
@@ -2,11 +2,19 @@ import XCTest
 @testable import FizBuzz
 
 struct MultiplesService {
-    
+    func isMultipleOfThree(int: Int) -> Bool  {
+        return false
+    }
 }
 
 class FizBuzzTests: XCTestCase {
     func testMultiplesServiceExists() {
         let service = MultiplesService()
+        XCTAssertNotNil(service)
+    }
+
+    func testMultipleOfThree() {
+        let service = MultiplesService()
+        XCTAssertFalse(service.isMultipleOfThree(int: 5))
     }
 }
