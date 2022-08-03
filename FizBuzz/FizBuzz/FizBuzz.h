ed42e4edd2805ecb48735e14f6f16fe05c4c7198
Paul Zabelin <paulz@users.noreply.github.com>
2022-08-03 15:37:52 -0700

    add initial project

---
+++ FizBuzz/FizBuzz/FizBuzz.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FizBuzz.
+FOUNDATION_EXPORT double FizBuzzVersionNumber;
+
+//! Project version string for FizBuzz.
+FOUNDATION_EXPORT const unsigned char FizBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizBuzz/PublicHeader.h>
+
+
